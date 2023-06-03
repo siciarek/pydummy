4381511c34c2687b72cb108c03215392ca6253ae
Jacek Siciarek <siciarek@gmail.com>
2023-06-03 16:51:36 +0200

    Set up dependencies

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup
 
-from tm_utils import __version__
+from tmutils import __version__
 
 setup(
-    name="tm-utils",
+    name="tmutils",
     version=__version__,
 
     url="https://github.com/siciarek/pydummy.git",
     author="Jacek Siciarek",
     author_email="siciarek@gmail.com",
     install_requires=[
-        "beautifulsoup4",
-        "python-docx",
+        "beautifulsoup4~=4.12.2",
+        "python-docx~=0.8.11",
+        "dependency-injector~=4.41.0",
+        "Jinja2~=3.1.2",
     ],
     py_modules=[
-        "tm_utils"
+        "tmutils"
     ],
 )
