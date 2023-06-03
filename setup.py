a3e349946d959c89d41eb76ee2cc69be9b114b64
Jacek Siciarek <siciarek@gmail.com>
2023-06-03 10:57:30 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -9,7 +9,10 @@ setup(
     url="https://github.com/siciarek/pydummy.git",
     author="Jacek Siciarek",
     author_email="siciarek@gmail.com",
-
+    install_requires=[
+        "beautifulsoup4",
+        "python-docx",
+    ],
     py_modules=[
         "pydummy"
     ],
